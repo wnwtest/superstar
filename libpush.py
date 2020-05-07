79809fd88083185c609456080e173f5b80e8f3e0
wnwtest <wnwtest@126.com>
2020-05-07 18:00:08 +0800

    简单写了写   push一堆 lib库到手机里的方法

---
+++ libpush.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 17:18:14 2020
+
+@author: Administrator
+"""
+
+import  os
+import time
+ 
+
+ 
+text = os.popen("adb root")
+time.sleep(1)
+print (text.read())
+
+text = os.popen("adb remount")
+time.sleep(1)
+print (text.read())
+
+flies = os.listdir(".");
+for ff in flies:
+    if (ff.endswith('.so')):
+        print ("adb push " + ff +" /system/vendor/lib/")
+        text = os.popen("adb push " + ff +" /system/vendor/lib/")
+        time.sleep(1)
+        print (text.read())
+
+text = os.popen("adb reboot")
+time.sleep(1)
+print (text.read())
