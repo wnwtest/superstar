8efa5d6cef3846e90d6b879574d8fdd5c879fa0d
tony <wangnanwang@unicair.cn>
2023-10-25 08:56:03 +0800

    取消对python 与git版本的依赖

--- 08-git push/upload.py
+++ 08-git push/upload.py
@@ -5,7 +5,8 @@
 # wangnanwang 2022 01
 # --------------------------------------------------------------
 
-import subprocess  
+import subprocess
+
 
 
 def test_echo():
@@ -18,20 +19,27 @@ def test_echo():
     return
 
 def get_output (cmd) :
-    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True) 
-    return result.stdout.decode().strip()
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    output, _ = process.communicate()
+    if process.returncode != 0:
+        raise Exception("Command failed: " + cmd)
+    return output.decode().strip()
+
 
 def get_push_output (branch ,caf) :
-    cmd = 'git push '+ caf + ' '+ 'HEAD:refs/for/'+branch
+    cmd = 'git push ' + caf + ' ' + 'HEAD:refs/for/' + branch
     print(cmd)
-    result = subprocess.run(cmd , stdout=subprocess.PIPE, shell=True, check=True) 
-    return result.stdout.decode()
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    output, _ = process.communicate()
+    if process.returncode != 0:
+        raise Exception("Command failed: " + cmd)
+    return output.decode()
 
 def do_git_push ():
-    branch = get_output('git branch --show-current')
+    branch = get_output('git rev-parse --abbrev-ref HEAD') 
     caf = get_output('git remote')
-    ret = get_push_output(branch,caf)
-    print (ret)
+    ret = get_push_output(branch, caf)
+    print(ret)
 
 if __name__ == '__main__':
 
