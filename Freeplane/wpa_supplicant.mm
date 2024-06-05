0ba4db383b69d907b24895948eba224e1bae03b2
tony <wangnanwang@unicair.cn>
2024-06-05 16:48:17 +0800

    0605 mm update

--- Freeplane/wpa_supplicant.mm
+++ Freeplane/wpa_supplicant.mm
@@ -139,8 +139,7 @@ CONFIG_EAP_AKA_PRIME=y</pre>
       
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="wpa_supplicant.conf" POSITION="top_or_left" ID="ID_284172741" CREATED="1715996065359" MODIFIED="1716275970130"><richcontent TYPE="DETAILS">
 <html>
@@ -172,8 +171,7 @@ network={
 &#x9;wpa_ptk_rekey=600
 }</pre>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="BoardConfig.mk" POSITION="top_or_left" ID="ID_649362084" CREATED="1715995970728" MODIFIED="1716190288783"><richcontent TYPE="DETAILS">
 <html>
@@ -245,8 +243,7 @@ network={
       BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node TEXT="支持WPA和IEEE 802.11i所定义的大部分功能" POSITION="bottom_or_right" ID="ID_1279048419" CREATED="1715996329118" MODIFIED="1715996338192"/>
@@ -273,8 +270,7 @@ network={
       <span style="color: #ffcc00; font-family: Times New Roman; font-size: 20px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 40px; text-transform: none; white-space: normal; word-spacing: 0px; display: inline !important; float: none;">（Protected Extensible Authentication Protocol，可扩展EAP） </span>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="EAP-TTLS" ID="ID_382774144" CREATED="1716181309460" MODIFIED="1716181561316"><richcontent TYPE="DETAILS">
 <html>
@@ -286,8 +282,7 @@ network={
       TTLS（Tunneled Transport Layer Security，隧道传输层安全协议）
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node TEXT="wpa_supplicant_8" POSITION="top_or_left" ID="ID_1239406848" CREATED="1716181729204" MODIFIED="1716190587873"><richcontent TYPE="DETAILS">
@@ -298,8 +293,7 @@ network={
   <body>
     <pre>wpa_supplicant and hostapd</pre>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="hostapd" ID="ID_404087880" CREATED="1716181745788" MODIFIED="1716181793303"><richcontent TYPE="DETAILS">
 <html>
   <head>
@@ -310,8 +304,7 @@ network={
       hostapd提供AP的功能
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="wpa_supplicant" ID="ID_433526271" CREATED="1716181813131" MODIFIED="1716181840656"><richcontent TYPE="DETAILS">
 <html>
@@ -323,8 +316,7 @@ network={
       Station模式,也叫Managed模式
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="src" ID="ID_244810068" CREATED="1716181888315" MODIFIED="1716181909829"><richcontent TYPE="DETAILS">
 <html>
@@ -336,8 +328,7 @@ network={
       hostapd和wpa_supplicant中都包含了一些通用的数据结构和处理方法
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node TEXT="wpa_cli(无界面的命令行程序)" POSITION="bottom_or_right" ID="ID_216154422" CREATED="1716186983708" MODIFIED="1716190558545"><richcontent TYPE="DETAILS">
@@ -353,8 +344,7 @@ network={
       生成wpa_supplicant,同时也会生成wpa_cli
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="wpa_supplicant_8_lib" POSITION="top_or_left" ID="ID_454360011" CREATED="1716190434694" MODIFIED="1716190486945"><richcontent TYPE="DETAILS">
 <html>
@@ -369,8 +359,7 @@ network={
       wpa_supplicant依赖的静态库
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="wpa_supplicant命令" POSITION="bottom_or_right" ID="ID_1073587356" CREATED="1716192246303" MODIFIED="1716192333456"><richcontent TYPE="DETAILS">
 <html>
@@ -382,8 +371,7 @@ network={
       在Android平台中,WPA Supplicant的客户端是位于Framework中的WifiService。用户在Settings界面进行Wi-Fi相关的操作最终都会经由WifiService通过发送命令的方式转交给wpa_supplicant去执行
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="PING" ID="ID_1798746114" CREATED="1716195285569" MODIFIED="1716195315771"><richcontent TYPE="DETAILS">
 <html>
   <head>
@@ -394,8 +382,7 @@ network={
       心跳检测命令。客户端用它判断WPAS是否工作正常
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT=" wpa_cli ping&#xa;Using interface &apos;wlan0&apos;&#xa;PONG" ID="ID_77626032" CREATED="1716202646303" MODIFIED="1716202683422"/>
 </node>
 <node TEXT="MIB" ID="ID_83575773" CREATED="1716201895014" MODIFIED="1716201913937"><richcontent TYPE="DETAILS">
@@ -408,8 +395,7 @@ network={
       客户端用该命令获取设备的MIB信息
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="wpa_cli mib&#xa;Using interface &apos;wlan0&apos;&#xa;dot11RSNAOptionImplemented=TRUE&#xa;dot11RSNAPreauthenticationImplemented=TRUE&#xa;dot11RSNAEnabled=TRUE&#xa;dot11RSNAPreauthenticationEnabled=TRUE&#xa;dot11RSNAConfigVersion=1&#xa;dot11RSNAConfigPairwiseKeysSupported=5&#xa;dot11RSNAConfigGroupCipherSize=128&#xa;dot11RSNAConfigPMKLifetime=43200&#xa;dot11RSNAConfigPMKReauthThreshold=70&#xa;dot11RSNAConfigNumberOfPTKSAReplayCounters=1&#xa;dot11RSNAConfigSATimeout=60&#xa;dot11RSNAAuthenticationSuiteSelected=00-0f-ac-2&#xa;dot11RSNAPairwiseCipherSelected=00-0f-ac-4&#xa;dot11RSNAGroupCipherSelected=00-0f-ac-4&#xa;dot11RSNAPMKIDUsed=&#xa;dot11RSNAAuthenticationSuiteRequested=00-0f-ac-2&#xa;dot11RSNAPairwiseCipherRequested=00-0f-ac-4&#xa;dot11RSNAGroupCipherRequested=00-0f-ac-4&#xa;dot11RSNAConfigNumberOfGTKSAReplayCounters=0&#xa;dot11RSNA4WayHandshakeFailures=0&#xa;dot1xSuppPaeState=5&#xa;dot1xSuppHeldPeriod=60&#xa;dot1xSuppAuthPeriod=30&#xa;dot1xSuppStartPeriod=30&#xa;dot1xSuppMaxStart=3&#xa;dot1xSuppSuppControlledPortStatus=Authorized&#xa;dot1xSuppBackendPaeState=2&#xa;dot1xSuppEapolFramesRx=0&#xa;dot1xSuppEapolFramesTx=2&#xa;dot1xSuppEapolStartFramesTx=0&#xa;dot1xSuppEapolLogoffFramesTx=0&#xa;dot1xSuppEapolRespFramesTx=0&#xa;dot1xSuppEapolReqIdFramesRx=0&#xa;dot1xSuppEapolReqFramesRx=0&#xa;dot1xSuppInvalidEapolFramesRx=0&#xa;dot1xSuppEapLengthErrorFramesRx=0&#xa;dot1xSuppLastEapolFrameVersion=0&#xa;dot1xSuppLastEapolFrameSource=00:00:00:00:00:00" ID="ID_1770267430" CREATED="1716202607558" MODIFIED="1716202610415"/>
 </node>
 <node TEXT="STATUS" ID="ID_488786155" CREATED="1716201935559" MODIFIED="1716201962402"><richcontent TYPE="DETAILS">
@@ -422,8 +408,7 @@ network={
       客户端用该命令来获取WPAS的工作状态
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="wpa_cli status&#xa;Using interface &apos;wlan0&apos;&#xa;wpa_state=DISCONNECTED&#xa;address=26:6c:25:90:43:4a&#xa;uuid=470bc95f-e93e-5861-b620-3e0fcc06b929" ID="ID_686330186" CREATED="1716202489967" MODIFIED="1716202493479"/>
 <node TEXT="wpa_cli status&#xa;Using interface &apos;wlan0&apos;&#xa;bssid=c8:bf:4c:77:58:fd&#xa;freq=5200&#xa;ssid=UNICAIR&#xa;id=0&#xa;id_str=%7B%22configKey%22%3A%22%5C%22UNICAIR%5C%22WPA_PSK%22%2C%22creatorUid%22%3A%221000%22%7D&#xa;mode=station&#xa;wifi_generation=5&#xa;pairwise_cipher=CCMP&#xa;group_cipher=CCMP&#xa;key_mgmt=WPA2-PSK&#xa;wpa_state=COMPLETED&#xa;ip_address=192.168.31.158&#xa;address=3a:30:f8:ed:ee:b2&#xa;uuid=470bc95f-e93e-5861-b620-3e0fcc06b929&#xa;ieee80211ac=1" ID="ID_1255245562" CREATED="1716202546982" MODIFIED="1716202550303"/>
 </node>
@@ -440,8 +425,7 @@ network={
       始)
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT=" wpa_cli add_network cts&#xa;Using interface &apos;wlan0&apos;&#xa;1" ID="ID_1254955050" CREATED="1716202748230" MODIFIED="1716202751399"/>
 </node>
 <node TEXT="SET_NETWORK&lt;network id&gt;&lt;variable&gt;&#xa;&lt;value&gt;" ID="ID_1632610554" CREATED="1716202009038" MODIFIED="1716202060002"><richcontent TYPE="DETAILS">
@@ -454,8 +438,7 @@ network={
       network id是无线网络的ID。此命令用于设置指定无线网络的信息。其中variable为参数名,value为参数的值
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="ENABLE_NETWORK&lt;network id&gt;" ID="ID_130087246" CREATED="1716202100423" MODIFIED="1716202127626"><richcontent TYPE="DETAILS">
 <html>
@@ -467,8 +450,7 @@ network={
       使能某个无线网络。此命令最终将促使WPAS发起一系列操作以加入该无线网络
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="Interactive Request" ID="ID_1193034609" CREATED="1716202169743" MODIFIED="1716202212737"><richcontent TYPE="DETAILS">
 <html>
@@ -480,8 +462,7 @@ network={
       WPAS主动给Client发送命令
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="CTRL-REQ-&lt;field name&gt;-&lt;network id&gt;-&lt;human readable text&gt;" ID="ID_1404774975" CREATED="1716202228086" MODIFIED="1716202262378"><richcontent TYPE="DETAILS">
 <html>
   <head>
@@ -492,8 +473,7 @@ network={
       CTRL-REQ-PASSWORD-0-Passwork needed for SSID test-network
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="CTRL-RSP-&lt;field name&gt;-&lt;network id&gt;-&lt;value&gt;" ID="ID_461831006" CREATED="1716202295911" MODIFIED="1716202303927"/>
 <node TEXT="CTRL-EVENT-&lt;field&gt;-&lt;text&gt;" ID="ID_1650177265" CREATED="1716202337646" MODIFIED="1716202339950"/>
@@ -509,8 +489,7 @@ network={
       LA.QSSI.14.0/LINUX/android/external/wpa_supplicant_8/src/common/wpa_ctrl.h
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="创建一个wpa控制端对象wpa_ctrl" ID="ID_1901880138" CREATED="1716273028911" MODIFIED="1716273058763"><richcontent TYPE="DETAILS">
 <html>
   <head>
@@ -524,8 +503,7 @@ network={
       void wpa_ctrl_close(struct wpa_ctrl *ctrl);
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT=" 客户端发送命令给wpa_supplicant" ID="ID_1628971161" CREATED="1716273084271" MODIFIED="1716273148707"><richcontent TYPE="DETAILS">
 <html>
@@ -537,8 +515,7 @@ network={
       int wpa_ctrl_request(struct wpa_ctrl *ctrl, const char*cmd, size_t cmd_len,char *reply, size_t *reply_len,void (*msg_cb)(char*msg, size_t len));
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="打开通知事件监听功能" ID="ID_847215028" CREATED="1716273677320" MODIFIED="1716273718636"><richcontent TYPE="DETAILS">
 <html>
@@ -559,8 +536,7 @@ network={
       int wpa_ctrl_recv(struct wpa_ctrl *ctrl, char *reply, size_t*reply_len);
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="客户端创建两个wpa_ctrl对象来简化自己的&#xa;逻辑处理。&#xa;·一个打开了通知事件监听功能的wpa_ctrl对象将只通过wpa_ctrl_recv来接收通知事件。&#xa;·另外一个wpa_ctrl专职用于发送命令和接收回复。由于没有调用wpa_ctrl_attach,故它不会收到通知事件" ID="ID_863305250" CREATED="1716273803159" MODIFIED="1716273825808"/>
 </node>
@@ -584,8 +560,7 @@ network={
     mkdir /data/vendor/wifi/wpa 0770 wifi wifi
     mkdir /data/vendor/wifi/wpa/sockets 0770 wifi wifi</pre>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="ifeq ($(WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY), true)&#xa;LOCAL_INIT_RC=aidl/android.hardware.wifi.supplicant-service.rc&#xa;endif" ID="ID_783512317" CREATED="1716274777464" MODIFIED="1716274780825">
 <node TEXT="service wpa_supplicant /vendor/bin/hw/wpa_supplicant \&#xa;&#x9;-O/data/vendor/wifi/wpa/sockets -puse_p2p_group_interface=1 -dd \&#xa;&#x9;-g@android:wpa_wlan0&#xa;&#x9;#   we will start as root and wpa_supplicant will switch to user wifi&#xa;&#x9;#   after setting up the capabilities required for WEXT&#xa;&#x9;#   user wifi&#xa;&#x9;#   group wifi inet keystore&#xa;&#x9;interface aidl android.hardware.wifi.supplicant.ISupplicant/default&#xa;&#x9;interface aidl vendor.qti.hardware.wifi.supplicant.ISupplicantVendor/default&#xa;&#x9;class main&#xa;&#x9;socket wpa_wlan0 dgram 660 wifi wifi&#xa;&#x9;disabled&#xa;&#x9;oneshot" ID="ID_1992255939" CREATED="1716275263553" MODIFIED="1716275267113"/>
 </node>
@@ -626,8 +601,7 @@ network={
       bss_no_flush_when_down=1
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="/data/vendor/wifi/wpa/wpa_supplicant.conf" ID="ID_1481341966" CREATED="1716279701747" MODIFIED="1716279704379"/>
 </node>
 <node TEXT="wpa_supplicant_init" ID="ID_1230052623" CREATED="1716447963562" MODIFIED="1716447966442">
@@ -643,8 +617,7 @@ network={
       wpa_driver_ops是driver i/f模块的核心数据结构,其内部定义了很多函数指针。而正是通过定义函数指针的方法,WPAS能够隔离上层使用者和具体的driver
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="wpa_driver_nl80211_ops" ID="ID_1154584448" CREATED="1716862700287" MODIFIED="1716862833798"><richcontent TYPE="DETAILS">
 <html>
@@ -659,8 +632,7 @@ network={
       kernel 层驱动的代理
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node TEXT="wpa_supplicant_add_iface" ID="ID_958782413" CREATED="1716863604968" MODIFIED="1716864504804"><richcontent TYPE="DETAILS">
@@ -671,8 +643,7 @@ network={
   <body>
     <pre><span class="c">Add a new network interface</span></pre>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="wpa_ssid" ID="ID_811225591" CREATED="1716874083543" MODIFIED="1716874087401">
 <node TEXT="passphrase" ID="ID_1338336830" CREATED="1717064774823" MODIFIED="1717064777681"/>
 <node TEXT="pairwise_cipher和group_cipher" ID="ID_980836326" CREATED="1717064798911" MODIFIED="1717064801471"/>
@@ -694,8 +665,7 @@ network={
     <pre><a class="l target selected" name="223" href="http://192.168.117.122:8080/source/xref/UPQ0090010-T10/LA.QSSI.14.0.r1/LINUX/android/external/wpa_supplicant_8/wpa_supplicant/Android.mk#223">
 </a><a href="http://192.168.117.122:8080/source/s?defs=L_CFLAGS&amp;project=UPQ0090010-T10" class="intelliWindow-symbol" data-definition-place="undefined-in-file">L_CFLAGS</a> += -<a href="http://192.168.117.122:8080/source/s?defs=DCONFIG_BACKEND_FILE&amp;project=UPQ0090010-T10" class="intelliWindow-symbol" data-definition-place="undefined-in-file">DCONFIG_BACKEND_FILE</a></pre>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node TEXT="wpa_driver_nl80211_init_nl_global" ID="ID_1542138160" CREATED="1717149215877" MODIFIED="1717149218764">
@@ -709,8 +679,7 @@ network={
       向wlan driver发送netlink消息
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="gobal-&gt;event" ID="ID_609990494" CREATED="1717149251508" MODIFIED="1717149265571"/>
 </node>
@@ -731,10 +700,10 @@ network={
       系统中后,rfkill才能对它们起作用)的工作以避免浪费电力
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="rfkill_init" ID="ID_947111878" CREATED="1717149694260" MODIFIED="1717149696884"/>
 </node>
+<node TEXT="wpa_driver_nl80211_get_info" ID="ID_882904578" CREATED="1717558306967" MODIFIED="1717558310759"/>
 </node>
 </node>
 </map>
