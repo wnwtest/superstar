96e54fd90837bca5ebd08e2fc9870e3d930a3989
tony <nanwang.wang@hotmail.com>
2024-09-25 15:48:16 +0800

    Android.mk update v2  20240925

--- 12-makefile-with-so/makefilev3-qfwy.py
+++ 12-makefile-with-so/makefilev3-qfwy.py
@@ -13,8 +13,14 @@ try:
 except NameError:
     pass  # Python 3
 
+def safe_makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+
 def sanitize_filename(filename):
-    # Remove any character that isn't a letter, number, underscore, or hyphen
     return re.sub(r'[^\w\-_\.]', '_', filename)
 
 def generate_android_mk(app_name, lib_files):
@@ -22,21 +28,24 @@ def generate_android_mk(app_name, lib_files):
 include $(CLEAR_VARS)
 LOCAL_MODULE := {0}
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := {1}.apk
+LOCAL_SRC_FILES := {0}.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/third-app
 LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_MODULE).apk
 LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_TARGET_ARCH := arm64
-LOCAL_MULTILIB := 64
-""".format(app_name, os.path.splitext(os.path.basename(app_name))[0])
+""".format(app_name)
 
     if lib_files:
+        config += "LOCAL_MULTILIB := both\n"
         config += "LOCAL_PREBUILT_JNI_LIBS := \\\n"
-        for lib_file in lib_files:
-            config += "\tlib/arm64-v8a/{} \\\n".format(lib_file)
+        for arch, libs in lib_files.items():
+            for lib in libs:
+                config += "\tlib/{}/{} \\\n".format(arch, lib)
+    else:
+        config += "LOCAL_MULTILIB := 64\n"
+        config += "LOCAL_MODULE_TARGET_ARCH := arm64\n"
 
     config += "\ninclude $(BUILD_PREBUILT)"
     return config
@@ -45,18 +54,23 @@ def process_apk(apk_path, output_dir):
     app_name = os.path.splitext(os.path.basename(apk_path))[0]
     safe_app_name = sanitize_filename(app_name)
     app_dir = os.path.join(output_dir, safe_app_name)
-    os.makedirs(app_dir, exist_ok=True)
+    safe_makedirs(app_dir)
 
     # Copy APK file
     shutil.copy2(apk_path, os.path.join(app_dir, safe_app_name + '.apk'))
 
     # Extract lib files if exist
-    lib_files = []
+    lib_files = {}
+    #architectures = ['arm64-v8a', 'armeabi-v7a', 'x86', 'x86_64']
+    architectures = ['arm64-v8a']
     with zipfile.ZipFile(apk_path, 'r') as zip_ref:
         for file in zip_ref.namelist():
-            if file.startswith('lib/arm64-v8a/'):
-                lib_files.append(os.path.basename(file))
-                zip_ref.extract(file, app_dir)
+            for arch in architectures:
+                if file.startswith('lib/{}/'.format(arch)):
+                    if arch not in lib_files:
+                        lib_files[arch] = []
+                    lib_files[arch].append(os.path.basename(file))
+                    zip_ref.extract(file, app_dir)
 
     # Generate Android.mk
     android_mk = generate_android_mk(safe_app_name, lib_files)
@@ -72,7 +86,7 @@ def main():
 
     apk_dir = sys.argv[1]
     output_dir = 'output'
-    os.makedirs(output_dir, exist_ok=True)
+    safe_makedirs(output_dir)
 
     preload_modules = []
 
